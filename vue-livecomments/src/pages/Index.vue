2e87bb9860ee16097db1d75cfdfa45a51de15988
Asuka Yagi <yagi.asuka.0x06@gmail.com>
2019-03-02 20:52:13 +0900

    Add vue.js

---
+++ vue-livecomments/src/pages/Index.vue
@@ -0,0 +1,53 @@
+<template>
+  <b-container class="h-100">
+    <b-row class="align-items-center h-100">
+      <b-col sm="6" class="mx-auto">
+        <b-card
+          title="LiveComments"
+          class="text-center">
+          <b-alert
+            :show="isError"
+            variant="danger"
+            dismissible
+            @dismissed="isError=false">
+            Connection failed.
+          </b-alert>
+          <b-button
+            variant="outline-primary"
+            size="sm"
+            @click="onClickNew"
+            :disabled="isDisabled">
+            <b-spinner small v-if="isDisabled"></b-spinner>
+            New Live
+          </b-button>
+        </b-card>
+      </b-col>
+    </b-row>
+  </b-container>
+</template>
+
+<script>
+import axios from 'axios'
+export default {
+  data () {
+    return {
+      isDisabled: false,
+      isError: false,
+    }
+  },
+  methods: {
+    onClickNew () {
+      this.isDisabled = true
+      window.setTimeout(this.newLive, 800)
+    },
+    newLive () {
+      axios.get('/api/newlive').then((r) => {
+        this.$router.puth({ path: `/live/${r.data.livekey}`})
+      }).catch(() => {
+        this.isError = true
+        this.isDisabled = false
+      })
+    }
+  },
+}
+</script>
---
+++ vue-livecomments/src/router.js
@@ -0,0 +1,14 @@
+import Vue from 'vue'
+import VueRouter from 'vue-router'
+
+import Index from './pages/Index.vue'
+import Live from './pages/Live.vue'
+
+Vue.use(VueRouter)
+
+export default new VueRouter ({
+  routes: [
+    { path: '/', component: Index },
+    { path: "/live/:id", component: Live },
+  ],
+})
